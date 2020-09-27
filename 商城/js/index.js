4cfdb31b50199a4e89f631a8336e5c80e21f71e7
haozi-233 <1084464977@qq.com>
2020-09-28 03:21:13 +0800

    1

--- 商城/js/index.js
+++ 商城/js/index.js
@@ -1,22 +1,56 @@
 (function () {
+    var thistime = document.getElementById('time');
+    var tutu = document.getElementById('tutu');
+    var yingying = document.getElementById('yingyong').children;
+    var lunhuanshu = 1;
+    var lunhuanqi;
     document.getElementById('soclick').onclick = function () {
         alert('搜索的内容是：' + document.getElementById('content').value)
     }
+    function jialing (a) {
+        if (a < 10) a = "0" + a;
+        return a
+    }
     function showTime () {
         var date = new Date();
-        var y = date.getFullYear()
-        var m = date.getMonth() + 1;
-        var d = date.getDate();
-        var h = date.getHours()
-        var min = date.getMinutes()
-        if (min < 10) min = "0" + min;
-        var s = date.getSeconds();
-        if (s < 10) s = "0" + s;
-        document.getElementById('time').innerHTML = y + "-" + m + "-" + d + "  " + h + " : " + min + " : " + s;
+        thistime.innerHTML = date.getFullYear() + "-" + jialing(date.getMonth() + 1) + "-" + jialing(date.getDate()) + "  " + jialing(date.getHours()) + " : " + jialing(date.getMinutes()) + " : " + jialing(date.getSeconds());
+    }
+    function biantu () {
+        tutu.setAttribute('src', `img/ad-0${lunhuanshu}.jpg`);
+        for (var a of yingying) {
+            a.classList.remove('huang');
+        }
+        yingying[lunhuanshu - 1].classList.add('huang');
+
+    }
+    for (var xuanfu of yingying) {
+        xuanfu.onmouseenter = function () {
+            clearInterval(lunhuanqi);
+            lunhuanshu = this.innerText * 1;
+            biantu();
+        }
+        xuanfu.onmouseleave = function () {
+            lunhuanqi = setInterval(function () {
+                if (lunhuanshu >= yingying.length) {
+                    lunhuanshu = 1;
+                } else {
+                    lunhuanshu++
+                }
+                biantu();
+            }, 3000);
+        }
     }
     window.onload = function () {
         showTime()
         setInterval(showTime, 1000);
+        lunhuanqi = setInterval(function () {
+            if (lunhuanshu >= yingying.length) {
+                lunhuanshu = 1;
+            } else {
+                lunhuanshu++
+            }
+            biantu();
+        }, 3000)
     }
 }())
 
